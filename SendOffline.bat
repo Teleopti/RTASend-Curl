267b19c56bce51796397ecc7f2cef149a450f60a
David Jonsson <david.jonsson@teleopti.com>
2018-07-09 10:04:01 -0600

    Add working example for a local Teleopti Demo

---
+++ SendOffline.bat
@@ -0,0 +1,12 @@
+::Pre-reqs: https://chocolatey.org/packages/curl
+@echo off
+SET ROOTDIR=%~dp0
+SET ROOTDIR=%ROOTDIR:~0,-1%
+SET payload=Offline.xml
+set curlExe=C:\ProgramData\chocolatey\lib\curl\tools\curl_x64\curl.exe
+set RTAUri=http://localhost/TeleoptiWFM/RTA/TeleoptiRtaService.svc
+
+"%curlExe%" -k -X POST -H "Content-Type: text/xml" -H "SOAPAction: \"http://tempuri.org/ITeleoptiRtaService/SaveBatchExternalUserState\"" --data-binary @"%ROOTDIR%\%payload%" %RTAUri%
+echo.
+echo %errorlevel%
+pause
