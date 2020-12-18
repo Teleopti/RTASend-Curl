90d21e7b2b3e02ec450f73ef6924548a8a6eed52
David Jonsson <david.jonsson@Calabrio.com>
2020-12-18 09:25:38 +0100

    Switch to latest choco of curl

--- SendStateBatch.bat
+++ SendStateBatch.bat
@@ -1,9 +1,10 @@
 ::Pre-reqs: https://chocolatey.org/packages/curl
+::install choco curl
 @echo off
 SET ROOTDIR=%~dp0
 SET ROOTDIR=%ROOTDIR:~0,-1%
 SET payload=StateBatch.xml
-set curlExe=C:\ProgramData\chocolatey\lib\curl\tools\curl_x64\curl.exe
+set curlExe=C:\ProgramData\chocolatey\lib\curl\tools\curl-7.72.0-win64-mingw\bin\curl.exe
 set RTAUri=http://localhost/TeleoptiWFM/RTA/TeleoptiRtaService.svc
 
 "%curlExe%" -k -X POST -H "Content-Type: text/xml" -H "SOAPAction: \"http://tempuri.org/ITeleoptiRtaService/SaveBatchExternalUserState\"" --data-binary @"%ROOTDIR%\%payload%" %RTAUri%
